272d5df90cd6a9dedfe64d7bd35f179477a86a29
Deepep <Hamzaini123@hotmail.com>
2020-09-26 17:11:39 -0400

    Working Camera

--- app/src/main/java/com/example/ramhack2020/MainActivity.kt
+++ app/src/main/java/com/example/ramhack2020/MainActivity.kt
@@ -1,14 +1,44 @@
 package com.example.ramhack2020
 
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    val CAMERA_REQUEST_CODE = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraButton.setOnClickListener {
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (callCameraIntent.resolveActivity(packageManager) != null){
+                startActivityForResult(callCameraIntent,CAMERA_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode,resultCode, data)
+
+        when(requestCode){
+            CAMERA_REQUEST_CODE ->{
+                if (resultCode == Activity.RESULT_OK && data != null){
+                    cameraImageView.setImageBitmap(data.extras!!.get("data") as Bitmap)
+                }
+            }
+            else -> {
+                Toast.makeText(this,"Unrecognized request code", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
