f7e707dadf48de3ac373bd783ec5595650c2b58d
Deepep <Hamzaini123@hotmail.com>
2020-09-26 20:34:40 -0400

    Completed High Res Camera

--- app/src/main/java/com/example/ramhack2020/MainActivity.kt
+++ app/src/main/java/com/example/ramhack2020/MainActivity.kt
@@ -4,15 +4,21 @@ package com.example.ramhack2020
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+private const val FILE_NAME = "photo.jpg"
+private const val CAMERA_REQUEST_CODE = 42
+private lateinit var photoFile: File
 
 class MainActivity : AppCompatActivity() {
-
-    val CAMERA_REQUEST_CODE = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,24 +26,32 @@ class MainActivity : AppCompatActivity() {
 
         cameraButton.setOnClickListener {
             val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            if (callCameraIntent.resolveActivity(packageManager) != null){
+            photoFile = getPhotoFile(FILE_NAME)
+
+            val fileProvider = FileProvider.getUriForFile(this,"com.example.ramhack2020.fileprovider", photoFile)
+            callCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+            if (callCameraIntent.resolveActivity(this.packageManager) != null){
                 startActivityForResult(callCameraIntent,CAMERA_REQUEST_CODE)
+            }else{
+                Toast.makeText(this,"Unable to open camera", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode,resultCode, data)
+    private fun getPhotoFile(fileName: String): File{
+        val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", storageDirectory)
+    }
 
-        when(requestCode){
-            CAMERA_REQUEST_CODE ->{
-                if (resultCode == Activity.RESULT_OK && data != null){
-                    cameraImageView.setImageBitmap(data.extras!!.get("data") as Bitmap)
-                }
-            }
-            else -> {
-                Toast.makeText(this,"Unrecognized request code", Toast.LENGTH_SHORT).show()
-            }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            /*val takenImage = data?.extras?.get("data") as Bitmap
+            cameraImageView.setImageBitmap(takenImage)*/
+            val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+            cameraImageView.setImageBitmap(takenImage)
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
         }
+
     }
 }
